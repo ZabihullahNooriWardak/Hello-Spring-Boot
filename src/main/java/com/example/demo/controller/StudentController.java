2c2570c15b4f42b3b43b4d972a82913a1ea8e9d6
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-10-12 12:17:09 +0430

    implement end point for update

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -12,8 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 
@@ -41,6 +43,14 @@ public ResponseEntity<List<Student>> getAllStudents() {
     return new ResponseEntity<>(students,HttpStatus.OK);
 }
 
-
+@PutMapping("/{id}")
+public ResponseEntity<Student> updateStudent(@PathVariable Long id,@RequestBody Student studentDetails ){
+    return studentRepository.findById(id).map(student ->{
+        student.setName(studentDetails.getName());
+        student.setEmail(studentDetails.getEmail());
+        Student updatestudent = studentRepository.save(student);
+        return ResponseEntity.ok(updatestudent);
+    }).orElseGet(()-> ResponseEntity.notFound().build());
+}
 
 }
