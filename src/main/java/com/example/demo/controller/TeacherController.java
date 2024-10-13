dff39626229e3cd0088cf47f9168fbd9c2d4b8b4
ZabihullahNooriWardak <zabinoori712@gmail.com>
2024-10-13 15:48:42 +0430

    make it clean

--- src/main/java/com/example/demo/controller/TeacherController.java
+++ src/main/java/com/example/demo/controller/TeacherController.java
@@ -1,5 +1,5 @@
 package com.example.demo.controller;
-import org.apache.el.stream.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.Student;
 import com.example.demo.entity.Teacher;
-import com.example.demo.repository.StudentRepository;
+
 import com.example.demo.repository.TeacherRepository;
-import jakarta.persistence.Id;
+
 import java.util.List;
 
 @RestController
@@ -41,14 +40,14 @@ public class TeacherController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Teacher> updateTeacher(@PathVariable Long id, @RequestBody Teacher teacherDetails){
-           return  teacherRepository.findById(id).map(teacher->{
+    public ResponseEntity<Teacher> updateTeacher(@PathVariable Long id, @RequestBody Teacher teacherDetails) {
+        return teacherRepository.findById(id).map(teacher -> {
             teacher.setName(teacherDetails.getName());
             teacher.setEmail(teacherDetails.getEmail());
             Teacher updateTeacher = teacherRepository.save(teacher);
             return ResponseEntity.ok(updateTeacher);
-           }).orElseGet(()->ResponseEntity.notFound().build());
-     
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+
     }
 
 }
